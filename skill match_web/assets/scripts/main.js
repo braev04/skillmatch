ab03e922ac373cd635465ef0a7a54b16dced8dec
braev04 <brayanecheveria04@gamil.com>
2026-07-20 17:56:33 -0300

    main hecho

---
+++ skill match_web/assets/scripts/main.js
@@ -0,0 +1,26 @@
+import { cargarVagas } from "./data.js";
+import { calcularMatch } from "./motor.js";
+import { renderCards } from "./ui.js";
+import { guardarPerfil } from "./storage.js";
+
+document.getElementById("formPerfil")
+.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  const perfil = {
+    nombre: nombre.value,
+    area: area.value,
+    skills: skills.value.split(",").map(s => s.trim())
+  };
+
+  guardarPerfil(perfil);
+
+  const vagas = await cargarVagas();
+
+  const resultados = vagas.map(v =>
+    calcularMatch(perfil, v)
+  );
+
+  renderCards(resultados);
+});
